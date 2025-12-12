da84e7fc87b064cab07351505c5c41707ce9fcd4
Gabriel Asankomah Gordon-Mensah <gordongabriel2004@gmail.com>
2025-12-12 01:16:32 +0000

    modified:   src/pages/home_sections.jsx

--- src/pages/home_sections.jsx
+++ src/pages/home_sections.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Search from '../assets/search_options.png';
 import Review from '../assets/Ratings.png';
 import Book from '../assets/book.png';
-import InstantQuotes from '../assets/instant qoutes.png';
+import InstantQuotes from '../assets/instant qoutes.jpg';
 import Carousel from '../components/carousal_slider.jsx';
 
 
