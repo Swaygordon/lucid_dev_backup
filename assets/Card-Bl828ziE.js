9be258eccae37a11b9d5009d15fdd4bc2d0e815d
Gabriel Asankomah Gordon-Mensah <gordongabriel2004@gmail.com>
2026-05-27 01:53:51 +0000

    Updates

---
+++ assets/Card-Bl828ziE.js
@@ -0,0 +1,5 @@
+import{j as s}from"./index-DaolUmlI.js";const t=({children:r,hoverable:a=!1,className:o="",...e})=>s.jsx("div",{className:`
+        bg-white dark:bg-[#1a1f2e] rounded-2xl p-6 shadow-md border border-gray-100 dark:border-[#1e293b]
+        ${a?"hover:shadow-xl hover:-translate-y-1 transition-all duration-300 cursor-pointer":""}
+        ${o}
+      `,...e,children:r});export{t as C};
