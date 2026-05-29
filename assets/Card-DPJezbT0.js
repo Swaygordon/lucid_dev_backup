8b3a10e6ea5bd1daecd462bb35d6eff4d7f687e8
Gabriel Asankomah Gordon-Mensah <gordongabriel2004@gmail.com>
2026-05-29 08:46:12 +0000

    Updates

--- assets/Card-DPJezbT0.js
+++
@@ -1,5 +0,0 @@
-import{j as s}from"./index-DM3xTRVJ.js";const t=({children:r,hoverable:a=!1,className:o="",...e})=>s.jsx("div",{className:`
-        bg-white dark:bg-[#1a1f2e] rounded-2xl p-6 shadow-md border border-gray-100 dark:border-[#1e293b]
-        ${a?"hover:shadow-xl hover:-translate-y-1 transition-all duration-300 cursor-pointer":""}
-        ${o}
-      `,...e,children:r});export{t as C};
---
+++ assets/trending-down-DWqFcmL2.js
@@ -0,0 +1,1 @@
+import{au as e,c as o}from"./index-D4TItlQ8.js";var a=e();const t=[["path",{d:"M16 17h6v-6",key:"t6n2it"}],["path",{d:"m22 17-8.5-8.5-5 5L2 7",key:"x473p"}]],n=o("trending-down",t);export{n as T,a as r};
